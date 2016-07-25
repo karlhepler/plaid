0304c8390ac8d2f862ab185e6bbb67447b252eed
Karl Hepler <khepler@brandmovers.com>
2016-07-25 17:11:08 -0400

    updated auth comments

--- src/OldTimeGuitarGuy/Plaid/Services/Auth.php
+++ src/OldTimeGuitarGuy/Plaid/Services/Auth.php
@@ -2,6 +2,18 @@
 
 namespace OldTimeGuitarGuy\Plaid\Services;
 
+/**
+ * The /auth endpoint allows you to collect a user's bank account and
+ * routing number, along with basic account data and balances. The
+ * product performs two crucial functions—it translates bank access
+ * credentials (username & password) into an accurate account and
+ * routing number. No input of account or routing number is necessary.
+ * Secondly it validates that this is the owner of this account number,
+ * in a NACHA compliant manner. No need for micro-deposits or any other
+ * secondary authentication.
+ *
+ * https://plaid.com/docs/api/#auth
+ */
 class Auth extends Base\Service
 {
     /**
